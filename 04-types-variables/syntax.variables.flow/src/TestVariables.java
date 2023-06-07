ed160acce8a9aad4a527c37821efebfa466c8ba9
emanoelcampos <emanoel.cv@gmail.com>
2023-06-07 20:16:14 -0300

    java type and variables

---
+++ 04-types-variables/syntax.variables.flow/src/TestVariables.java
@@ -0,0 +1,17 @@
+public class TestVariables {
+
+    public static void main(String[] args) {
+
+        int age;
+        age = 26;
+        System.out.println(age);
+
+        age = 30 + 10;
+        System.out.println(age);
+
+        age = (7 * 5) + 2;
+        System.out.println(age);
+
+        System.out.println("The age is " + age);
+    }
+}
