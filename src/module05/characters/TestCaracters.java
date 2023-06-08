88bc9c4974f439fbe52f8a8fed40dac8849f0cfb
emanoelcampos <emanoel.cv@gmail.com>
2023-06-07 22:54:49 -0300

    working with caracters

---
+++ src/module05/characters/TestCaracters.java
@@ -0,0 +1,23 @@
+package module05.characters;
+
+public class TestCaracters {
+
+    public static void main(String[] args) {
+
+        char letter = 'e';
+        System.out.println(letter);
+
+        char value = 69;
+        System.out.println(value);
+
+        value = (char) (value - 1);
+        System.out.println(value);
+
+        String word = "Alura online technology courses.";
+        System.out.println(word);
+
+        word = word + 2023;
+        System.out.println(word);
+
+    }
+}
---
+++ src/module05/characters/TestValues.java
@@ -0,0 +1,15 @@
+package module05.characters;
+
+public class TestValues {
+
+    public static void main(String[] args) {
+
+        int first = 5;
+        int second = 7;
+
+        second = first;
+        first = 10;
+
+        System.out.println(second);
+    }
+}
