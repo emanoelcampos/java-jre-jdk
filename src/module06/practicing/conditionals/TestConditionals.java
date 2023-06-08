3305bf864c3d2ae3b0419e119cd96b4a643662cd
emanoelcampos <emanoel.cv@gmail.com>
2023-06-07 23:28:45 -0300

    if conditionals

---
+++ src/module06/practicing/conditionals/TestConditionals.java
@@ -0,0 +1,23 @@
+package module06.practicing.conditionals;
+
+public class TestConditionals {
+
+    public static void main(String[] args) {
+
+        System.out.println("Testing Conditionals");
+
+        int age = 17;
+        int amountPeople = 3;
+
+        if(age >= 18) {
+            System.out.println("Adult");
+        } else {
+            if(amountPeople >= 2) {
+                System.out.println("Minor but accompanied by an adult");
+            } else {
+                System.out.println("Minor");
+            }
+
+        }
+    }
+}
